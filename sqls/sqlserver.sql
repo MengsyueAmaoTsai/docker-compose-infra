6e5889606f36119c1cbe953984e6846bd8562df3
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-07-03 02:13:01 +0800

    update sqlserver.sql

--- sqls/sqlserver.sql
+++ sqls/sqlserver.sql
@@ -3,6 +3,7 @@ CREATE DATABASE test COLLATE Chinese_PRC_CI_AS;
 
 CREATE LOGIN msat7201 WITH PASSWORD = 'Among7201';
 ALTER SERVER ROLE sysadmin ADD MEMBER msat7201;
+USE master;
 ALTER AUTHORIZATION ON DATABASE::test TO msat7201;
 
 USE test;
@@ -12,8 +13,13 @@ CREATE TABLE bots (
     side VARCHAR(8) NOT NULL,
     type VARCHAR(32) NOT NULL,
     platform VARCHAR(64) NOT NULL,
-    tags VARCHAR(max)
+    tags VARCHAR(max),
+    CONSTRAINT CK_Side CHECK (side IN ('Long', 'Short')),
+    CONSTRAINT CK_BotType CHECK (type IN ('Intraday', 'Position')),
+    CONSTRAINT CK_TradingPlatform CHECK (platform IN (
+        'XQ', 'MultiCharts', 'TradeStation', 
+        'MultiCharts.NET', 'Quantower', 'QuantConnect', 
+        'CTrader', 'NinjaTrader', 'WealthLab', 
+        'TradingView', 'VeighNa'))
 );
-
-
 
