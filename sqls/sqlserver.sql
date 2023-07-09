33950ca646a4648e989bb825d9b6ec07b7ca749d
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2023-07-10 06:59:35 +0800

    update sql

--- sqls/sqlserver.sql
+++ sqls/sqlserver.sql
@@ -8,18 +8,86 @@ ALTER AUTHORIZATION ON DATABASE::test TO msat7201;
 
 USE test;
 
-CREATE TABLE bots (
-    id VARCHAR(64) PRIMARY KEY,
-    side VARCHAR(8) NOT NULL,
-    type VARCHAR(32) NOT NULL,
-    platform VARCHAR(64) NOT NULL,
-    tags VARCHAR(max),
-    CONSTRAINT CK_Side CHECK (side IN ('Long', 'Short')),
-    CONSTRAINT CK_BotType CHECK (type IN ('Intraday', 'Position')),
-    CONSTRAINT CK_TradingPlatform CHECK (platform IN (
-        'XQ', 'MultiCharts', 'TradeStation', 
-        'MultiCharts.NET', 'Quantower', 'QuantConnect', 
-        'CTrader', 'NinjaTrader', 'WealthLab', 
-        'TradingView', 'VeighNa'))
+CREATE TABLE instruments (
+    symbol VARCHAR(64) PRIMARY KEY,
+    ticker VARCHAR(16),
+    description VARCHAR(256),
+    type VARCHAR(16) CHECK (type IN ('Equity', 'Future', 'Cryptocurrency')), 
+    exchange VARCHAR(16)
 );
 
+CREATE TABLE accounts (
+    id VARCHAR(128) PRIMARY KEY UNIQUE,
+    name VARCHAR(64) UNIQUE,
+    primary_currency VARCHAR(8) CHECK (currency IN ('TWD', 'USDT', 'USD', 'EUR')),
+
+    created_at DATETIME2(7) NOT NULL DEFAULT SYSUTCDATEIME() 
+);
+
+CREATE TABLE balances (
+    account_id VARCHAR(128),
+    amount DECIMAL(18, 2),
+    currency VARCHAR(8) CHECK (currency IN ('TWD', 'USDT', 'USD', 'EUR')),
+
+    FOREIGN KEY (account_id) REFERENCES accounts (id)
+);
+
+CREATE TABLE orders (
+    account_id VARCHAR(128),
+    id VARCHAR(128) PRIMARY KEY,
+    symbol VARCHAR(64),
+    trade_type VARCHAR(8) CHECK (trade_type IN ('Buy', 'Sell')),
+    quantity DECIMAL(18, 2),
+    price DECIMAL(18, 2),
+    status VARCHAR(16) CHECK (status IN ('Pending', 'Cancelled', 'Rejected', 'Filled')),
+    time_in_force VARCHAR(8) CHECK (time_in_force IN ('IOC')),
+    created_at DATETIME2(7) NOT NULL DEFAULT SYSUTCDATETIME(),
+
+    FOREIGN KEY (account_id) REFERENCES accounts (id),
+    FOREIGN KEY (symbol) REFERENCES instruments (symbol)
+);
+
+CREATE TABLE executions (
+    account_id VARCHAR(128),
+    order_id VARCHAR(128),
+    symbol VARCHAR(64),
+    trade_type VARCHAR(8) CHECK (trade_type IN ('Buy', 'Sell')),
+    executed_quantity DECIMAL(18, 2),
+    executed_price DECIMAL(18, 2),
+    executed_time DATETIME2(7) NOT NULL DEFAULT SYSUTCDATETIME(),
+    
+    FOREIGN KEY (account_id) REFERENCES accounts (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id),
+    FOREIGN KEY (symbol) REFERENCES instruments (symbol)
+);
+
+CREATE TABLE positions (
+    account_id VARCHAR(128),
+    symbol VARCHAR(64),
+    side VARCHAR(8) CHECK (trade_type IN ('Long', 'Short')),
+
+    open_time DATETIME2(7) NOT NULL DEFAULT SYSUTCDATETIME(),
+    avg_price DECIMAL(18, 2),    
+    unrealized_pnl DECIMAL(18, 2),    
+    
+    FOREIGN KEY (account_id) REFERENCES accounts (id),
+    FOREIGN KEY (symbol) REFERENCES instruments (symbol)
+);
+
+CREATE TABLE trades (
+    account_id VARCHAR(128),
+    symbol VARCHAR(64),
+    side VARCHAR(8) CHECK (trade_type IN ('Long', 'Short')),
+
+    open_time DATETIME2(7) NOT NULL DEFAULT SYSUTCDATETIME(),
+    open_avg_price DECIMAL(18, 2),    
+    close_time DATETIME2(7) NOT NULL DEFAULT SYSUTCDATETIME(),
+    close_avg_price DECIMAL(18, 2),    
+
+    pnl DECIMAL(18, 2),
+    fees DECIMAL(18, 2),
+    net_profit DECIMAL(18, 2),
+    
+    FOREIGN KEY (account_id) REFERENCES accounts (id),
+    FOREIGN KEY (symbol) REFERENCES instruments (symbol)
+);
